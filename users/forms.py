8fd7cb3ebae2d860984270e0c1434d09253e9e0a
Aidar Putilov <putiloff@gmail.com>
2024-09-24 23:02:28 +0500

    Fix profile

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from users.models import User
@@ -13,3 +14,8 @@ class UserProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ("email", "phone", "country", "avatar")
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].widget = forms.HiddenInput()
