191c8603cb5757eea6b15808f3903f6c22147e9f
Aidar Putilov <putiloff@gmail.com>
2024-09-29 22:01:37 +0500

    Edit csu function

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,17 +5,34 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        user = User.objects.create(email="admin@sky.pro")
-        user.set_password("123qwe")
-        user.is_active = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
-        user = User.objects.create(email="user1@sky.pro")
-        user.set_password("123qwe")
-        user.is_active = True
-        user.save()
-        user = User.objects.create(email="user2@sky.pro")
-        user.set_password("123qwe")
-        user.is_active = True
-        user.save()
+        emails = [
+            "admin@sky.pro",
+            "user1@sky.pro",
+            "user2@sky.pro"
+        ]
+        for email_str in emails:
+            #User.objects.filter(email=email_str).exists()
+            if not User.objects.filter(email=email_str).exists():
+                user = User.objects.create(email=email_str)
+                user.set_password("123qwe")
+                user.is_active = True
+                if 'admin' in email_str:
+                    user.is_staff = True
+                    user.is_superuser = True
+                user.save()
+
+
+#        user = User.objects.create(email="admin@sky.pro")
+#        user.set_password("123qwe")
+#        user.is_active = True
+#        user.is_staff = True
+#        user.is_superuser = True
+#        user.save()
+#        user = User.objects.create(email="user1@sky.pro")
+#        user.set_password("123qwe")
+#        user.is_active = True
+#        user.save()
+#        user = User.objects.create(email="user2@sky.pro")
+#        user.set_password("123qwe")
+#        user.is_active = True
+#        user.save()
